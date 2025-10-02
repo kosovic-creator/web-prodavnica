236c335fa2245b4addb449311569694a9ca7e35f
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-03 01:07:11 +0200

    ukllonjen feath Proizvod optamilizacija i dalje

--- lib/fetchProizvod.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchProizvod(id: string, lang: string) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/proizvodi/prevodi/${id}/${lang}`,
-    { cache: 'no-store' }
-  );
-  if (!res.ok) throw new Error('Greška pri preuzimanju proizvoda');
-  return res.json();
-}
