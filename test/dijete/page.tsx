ade8c9bc4f191edecef8ab808793e8c6e64e2f5e
kosovic-creator <drasko.kosovic@icloud.com>
2025-10-09 09:28:37 +0200

    init

--- test/dijete/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-interface ChildProps {
-    value: string;
-    onChange: (val: string) => void;
-}
-const Child = ({ value, onChange }: ChildProps) => (
-    <input value={value} onChange={e => onChange(e.target.value)} />
-);
-export default Child;
